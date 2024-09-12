36ceb50b31e68a5cabe1398cf0bd20dfa7a65aa5
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2024-09-12 08:43:09 -0300

    :package: package: Downgrade package versions

---
+++ Sources/App/Migrations/CreateUserTableMigration.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by Josileudo on 10/09/24.
+//
+
+import Foundation
+import Fluent
+import Vapor
+
+struct CreateUserTableMigrations: AsyncMigration {
+    func prepare(on database: FluentKit.Database) async throws {
+        try await database.schema("users")
+            .id()
+            .field("email", .string, .required)
+            .field("password", .string, .required)
+            .unique(on: "email")
+            .create()
+        
+    }
+
+    func revert(on database: FluentKit.Database) async throws {
+        try await database.schema("users")
+            .delete()
+    }
+}
---
+++ Sources/App/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Josileudo on 10/09/24.
+//
+
+import Foundation
